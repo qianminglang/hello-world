ebb534a8f04f2bac42bf551d3c8483426452c5df
钱明浪 <569040804@qq.com>
2022-02-25 18:02:01 +0800

    整合resilience4j基础的api

---
+++ resilience4j/src/main/java/org/example/Resilience4j.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Resilience4j
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Resilience4j.class, args);
+    }
+}
