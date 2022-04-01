3a3c47ab5dfe3585a1c8af9fd85ca4553b147357
钱明浪 <569040804@qq.com>
2022-04-01 22:27:50 +0800

    sharding-jdbc使用自定义的id生成器插入表作为主键1

---
+++ sharding-jdbc/src/main/java/com/clear/config/SelfGeneratorId.java
@@ -0,0 +1,33 @@
+package com.clear.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.core.strategy.keygen.SnowflakeShardingKeyGenerator;
+import org.apache.shardingsphere.spi.keygen.ShardingKeyGenerator;
+
+import java.util.Properties;
+
+@Slf4j
+public class SelfGeneratorId implements ShardingKeyGenerator {
+    private static final SnowflakeShardingKeyGenerator snowflakeShardingKeyGenerator = new SnowflakeShardingKeyGenerator();
+
+    @Override
+    public Comparable<?> generateKey() {
+        log.info("使用雪花算法作为自己得id生成器");
+        return snowflakeShardingKeyGenerator.generateKey();
+    }
+
+    @Override
+    public String getType() {
+        return "LAGOUID";
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
