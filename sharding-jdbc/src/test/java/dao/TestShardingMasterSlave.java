419423e9d317f65657f7e38772dead8aca5f2409
钱明浪 <569040804@qq.com>
2022-04-05 10:56:57 +0800

    sharding-jdbc读写分离

---
+++ sharding-jdbc/src/test/java/dao/TestShardingMasterSlave.java
@@ -0,0 +1,41 @@
+package dao;
+
+import com.clear.RunBoot;
+import com.clear.dao.CityRepository;
+import com.clear.entity.City;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+//注解的意义在于Test测试类要使用注入的类，比如@Autowired注入的类，
+@RunWith(SpringRunner.class)
+//通过RunBoot来创建容器
+@SpringBootTest(classes = RunBoot.class)
+public class TestShardingMasterSlave {
+
+
+    @Resource
+    CityRepository cityRepository;
+
+    @Test
+    public void add() {
+        City city = new City();
+        city.setName("shanghai");
+        city.setProvince("shanghai");
+        cityRepository.save(city);
+    }
+
+
+    @Test
+    public void masterSlave() {
+        List<City> all = cityRepository.findAll();
+        for (City city : all) {
+            System.out.println(city);
+        }
+    }
+
+}
